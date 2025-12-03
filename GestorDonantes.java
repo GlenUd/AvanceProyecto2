454de8ed5e5bdbc449e4625cdea9fff0ca8d8ad7
Oliver99f <oliver.quila@udla.edu.ec>
2025-12-03 14:45:05 -0500

    Add files via upload

--- GestorDonantes.java
+++ GestorDonantes.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class GestorDonantes {
     private List<Donante> donantes = new ArrayList<>();
     public GestorDonantes() {
-        donantes.add(new Donante("Ana Pérez",  "0102030405", "O+", 30, "Quito",      "2025-10-01"));
-        donantes.add(new Donante("Luis Gómez", "1102233445", "A-", 40, "Guayaquil", "2025-09-15"));
-        donantes.add(new Donante("María Ruiz","0912345678", "B+", 28, "Cuenca",     "2025-10-10"));
-        donantes.add(new Donante("Carlos Vega","1723456789", "AB+",35,"Ambato",     "2025-08-30"));
-        donantes.add(new Donante("Sofía León","0609876543", "O-", 25, "Quito",      "2025-10-05"));
+        donantes.add(new Donante("Ana Pérez",  "0102030405", "O+", 30, "El Inca",      "2025-10-01"));
+        donantes.add(new Donante("Luis Gómez", "1102233445", "A-", 40, "Batan", "2025-09-15"));
+        donantes.add(new Donante("María Ruiz","0912345678", "B+", 28, "Carolina Alto",     "2025-10-10"));
+        donantes.add(new Donante("Carlos Vega","1723456789", "AB+",35,"Monteserrin",     "2025-08-30"));
+        donantes.add(new Donante("Sofía León","0609876543", "O-", 25, "Iñaquito",      "2025-10-05"));
     }
 
     public void agregarDonante(Donante d) {
